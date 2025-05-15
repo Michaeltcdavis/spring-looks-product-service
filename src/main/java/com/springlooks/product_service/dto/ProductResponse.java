5ef36e492388fec95597028894bfcacd73d7e00e
Michael Davis <michaeltcdavis@gmail.com>
2025-05-14 21:05:11 -0400

    remove user/pass

---
+++ src/main/java/com/springlooks/product_service/dto/ProductResponse.java
@@ -0,0 +1,4 @@
+package com.springlooks.product_service.dto;
+
+public record ProductResponse() {
+}
---
+++ src/main/java/com/springlooks/product_service/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.springlooks.product_service.service;
+
+public class ProductService {
+}
